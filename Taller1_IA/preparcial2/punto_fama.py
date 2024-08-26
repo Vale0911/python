1b943f4d8a5488d2571c24234c736cdce40908fd
Valentina Mesa Fajardo <147108515+Vale0911@users.noreply.github.com>
2024-08-26 10:37:05 -0500

    Update punto_fama.py

--- Taller1_IA/preparcial2/punto_fama.py
+++ Taller1_IA/preparcial2/punto_fama.py
@@ -1,4 +1,4 @@
-import sys
+
 secreto = int(input("Juagdor A, ingrese el número secreto de dos dígitos:"))
 
 if secreto < 10 or secreto >99:
@@ -11,69 +11,67 @@ else :
     digito_intento1= intento //10
     digito_intento2 = intento % 10
 
-    if intento == secreto:
-        print("Usted ha encontrado en número secreto!")
-        sys.exit ()
-    elif digito_intento1 == digito_intento2 and digito_intento2 == digito_secreto1:
-        print('Usted tiene 2 puntos')
+    if digito_intento1 == digito_secreto1 and digito_intento2 == digito_secreto2:#type: ignore
+        print("Usted ha encontrado el número secreto!")
     elif digito_intento1 == digito_secreto1 or digito_intento2 == digito_secreto2:
         print("Usted tiene 1 fama")
     elif digito_intento1 == digito_secreto2 or digito_intento2== digito_secreto1:
         print("Usted tiene 1 punto")
-    
-    intento =int(input("Juagdor B, ingrese su segundo intento"))
-
-    digito_intento1 = intento // 10
-    digito_intento2 = intento % 10
-
-    if intento == secreto:
-        print("Usted ha encontrado el número secreto!")
-        sys.exit ()
-    elif digito_intento1 == digito_intento2 and digito_intento2 == digito_secreto1:
-        print("Usted tiene 2 puntos")
-    elif digito_intento1 == digito_secreto1 or digito_intento2 == digito_secreto2:
-        print("Usted tiene 1 fama")
-    elif digito_intento1 == digito_secreto2 or digito_intento2== digito_secreto1:
-        print("Usted tiene 1 punto")
-    
-    intento =int(input("Juagdor B, ingrese su tercer intento"))
-
-    digito_intento1 = intento // 10
-    digito_intento2 = intento % 10
-
-    if intento == secreto:
-        print("Usted ha encontrado el número secreto!")
-        sys.exit ()
-    elif digito_intento1 == digito_intento2 and digito_intento2 == digito_secreto1:
-        print("Usted tiene 2 puntos")
-    elif digito_intento1 == digito_secreto1 or digito_intento2 == digito_secreto2:
-        print("Usted tiene 1 fama")
-    elif digito_intento1 == digito_secreto2 or digito_intento2== digito_secreto1:
-        print("Usted tiene 1 punto")
-    
-    intento =int(input("Juagdor B, ingrese su cuarto intento"))
-
-    digito_intento1 = intento // 10
-    digito_intento2 = intento % 10
-
-    if intento == secreto:
-        print("Usted ha encontrado el número secreto!")
-        sys.exit ()
-    elif digito_intento1 == digito_intento2 and digito_intento2 == digito_secreto1:
-        print("Usted tiene 2 puntos")
-    elif digito_intento1 == digito_secreto1 or digito_intento2 == digito_secreto2:
-        print("Usted tiene 1 fama")
-    elif digito_intento1 == digito_secreto2 or digito_intento2== digito_secreto1:
-        print("Usted tiene 1 punto")
-    
-    intento =int(input("Juagdor B, ingrese su quinto intento"))
-
-    digito_intento1 = intento // 10
-    digito_intento2 = intento % 10
-
-    if intento == secreto:
-        print("Usted ha encontrado el número secreto!")
     else:
-        print("Juagdor B, ha perdido. El número era:",secreto)
+    
+         intento =int(input("Juagdor B, ingrese su segundo intento"))
+
+         digito_intento1 = intento // 10
+         digito_intento2 = intento % 10
+
+         if digito_intento1 == digito_secreto1 and digito_intento2 == digito_secreto2:#type: ignore
+            print("Usted ha encontrado el número secreto!")
+         elif digito_intento1 == digito_secreto1 or digito_intento2 == digito_secreto2:
+             print("Usted tiene 1 fama")
+         elif digito_intento1 == digito_secreto2 or digito_intento2== digito_secreto1:
+             print("Usted tiene 1 punto")
+         else:
+    
+             intento =int(input("Juagdor B, ingrese su tercer intento"))
+
+             digito_intento1 = intento // 10
+             digito_intento2 = intento % 10
+
+             if digito_intento1 == digito_secreto1 and digito_intento2 == digito_secreto2:#type: ignore
+                 print("Usted ha encontrado el número secreto!")
+             elif digito_intento1 == digito_secreto1 or digito_intento2 == digito_secreto2:
+                 print("Usted tiene 1 fama")
+             elif digito_intento1 == digito_secreto2 or digito_intento2== digito_secreto1:
+                print("Usted tiene 1 punto")
+             else:
+    
+                 intento =int(input("Juagdor B, ingrese su cuarto intento"))
+
+                 digito_intento1 = intento // 10
+                 digito_intento2 = intento % 10
+
+                 if digito_intento1 == digito_secreto1 and digito_intento2 == digito_secreto2:#type: ignore
+                    print("Usted ha encontrado el número secreto!")
+                 elif digito_intento1 == digito_secreto1 or digito_intento2 == digito_secreto2:
+                    print("Usted tiene 1 fama")
+                 elif digito_intento1 == digito_secreto2 or digito_intento2== digito_secreto1:
+                    print("Usted tiene 1 punto")
+                 else: 
+    
+                  intento =int(input("Juagdor B, ingrese su quinto intento"))
+
+                  digito_intento1 = intento // 10
+                  digito_intento2 = intento % 10
+                  
+                 if digito_intento1 == digito_secreto1 and digito_intento2 == digito_secreto2:#type: ignore
+                    print("Usted ha encontrado el número secreto!")
+                 elif digito_intento1 == digito_secreto1 or digito_intento2 == digito_secreto2:
+                    print("Usted tiene 1 fama")
+                 elif digito_intento1 == digito_secreto2 or digito_intento2== digito_secreto1:
+                    print("Usted tiene 1 punto")
+    
+                 else:
+                    print("Juagdor B, ha perdido. El número era:",secreto)
+    
     
     
