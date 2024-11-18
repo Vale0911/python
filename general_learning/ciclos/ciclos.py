2cda349baf7d54fdb9d4525b6c7da1b0a08bcb63
valentina mesa <valemf0911@gmail.com>
2024-11-18 18:52:32 -0500

    nueva carpeta

---
+++ general_learning/ciclos/ciclos.py
@@ -0,0 +1,32 @@
+# Escribe un programa que imprima los números del 1 al 10 usando un ciclo for.
+"""for i in range (10):
+    print (i) """
+
+# Escribe un programa que imprima los números del 1 al 10 usando un ciclo while.
+"""i = 0
+    while i < 10:
+        print (i)
+    i+=1
+"""
+# o asi:
+"""i=0
+while true:
+    print (i)
+    i+=1
+    if i <= 10:
+        break"""
+# Escribe un programa que imprima los números impares entre 1 y 20 usando un ciclo for.
+"""for i in range(1, 21):
+    if i % 2 != 0:
+        print(i)"""
+# Escribe un programa que imprima todos los números divisibles por 5 entre 1 y 50 usando un ciclo while.
+i=1 
+while i<=50:
+    if i %5 == 0:
+        print(i)
+    i+=1
+"""suma = 0
+
+for numero in range (1,100):
+    suma+=numero
+print("El resultado es:",suma)"""
